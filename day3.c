d233ee14e9777eb95c91e782c2042ac553c1723a
Rui Da Silva Figueiredo De Campos <ruida-si@c1r11s8.42porto.com>
2024-12-07 16:32:27 +0000

    day3 done

--- day3.c
+++ day3.c
@@ -21,11 +21,13 @@ int		get_numbers(char *s, int *i);
 int		compare(char *comp, char *s);
 char	*copy_last(char *s);
 char	*join(char *last, char *s);
-int		get_string(char *s, char *last);
+int		get_string(char *s, char *last, int *sum2);
+int		check_dont(char *dest, char *comp, int *sum3, int index);
 
 int	main()
 {
 	int sum = 0;
+	int sum2 = 0;
 	char s[31];
 	int fd = open("input3", O_RDONLY);
 	if (fd < 0)
@@ -41,18 +43,21 @@ int	main()
 		if (b_read <= 0)
 			break ;
 		s[b_read] = '\0';
-		sum += get_string(s, last);
+		sum += get_string(s, last, &sum2);
 		last = copy_last(s);
 	}
 	free(last);
 	printf("\033[1;32mFirst answer:\033[0m %i\n", sum);
+	printf("\033[1;32mSecond answer:\033[0m %i\n", sum - sum2);
 	close(fd);
 }
 
-int	get_string(char *s, char *last)
+int	get_string(char *s, char *last, int *sum2)
 {
 	int sum = 0;
+	int sum3 = 0;
 	static int n;
+	static int dont;
 	char *dest = NULL;
 
 	if (!n)
@@ -64,10 +69,43 @@ int	get_string(char *s, char *last)
 	{
 		dest = join(last, s);
 		sum += get_line(dest);
+		if (dont)
+		{
+			if(check_dont(dest, "do()", &sum3, 1))
+			{
+				dont = 0;
+			}
+			*sum2 += sum + sum3;
+		}
+		else
+		{
+			if (check_dont(dest, "don't()", &sum3, 2))
+			{
+				dont = 1;
+				*sum2 += sum3;
+			}
+		}
 		free(dest);
 		free(last);
 	}
 	return (sum);
+}
+
+int	check_dont(char *dest, char *comp, int *sum3, int index)
+{
+	while (*dest)
+	{
+		if (compare(comp, dest))
+		{
+			if (index == 2)
+				*sum3 += get_line(dest);
+			if (index == 1)
+				*sum3 -= get_line(dest);
+			return (1);
+		}
+		dest++;
+	}
+	return (0);
 }
 
 char *copy_last(char *s)
